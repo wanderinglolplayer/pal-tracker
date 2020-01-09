ac3fd83deb41ba7847db1413b3f5631ead61f1a9
William <wmhufnagle@gmail.com>
2020-01-09 11:51:29 -0500

    work for Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -10,9 +14,12 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
     }
     @Bean
     public Person person() {
